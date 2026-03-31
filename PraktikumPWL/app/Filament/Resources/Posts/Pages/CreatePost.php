d5d4c9e434eb468eb180181431a5b27a3adb4de6
nadyaaurora <nadyaaurora575@gmail.com>
2026-03-31 21:08:03 +0700

    feat: tambah redirect ke halaman index setelah simpan post

--- PraktikumPWL/app/Filament/Resources/Posts/Pages/CreatePost.php
+++ PraktikumPWL/app/Filament/Resources/Posts/Pages/CreatePost.php
@@ -9,5 +9,10 @@ use Illuminate\Support\Facades\Auth;
 class CreatePost extends CreateRecord
 {
     protected static string $resource = PostResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
 
