bb8893bde0a60ee332ced2b86800b98e01400e50
nadyaaurora <nadyaaurora575@gmail.com>
2026-04-11 10:08:11 +0700

    feat : product info, pricing, stock

--- PraktikumPWL/app/Filament/Resources/Products/Schemas/ProductForm.php
+++ PraktikumPWL/app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -3,6 +3,13 @@
 namespace App\Filament\Resources\Products\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Wizard;
+use Filament\Schemas\Components\Wizard\Step;
+use Filament\Schemas\Components\Group;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Checkbox;
 
 class ProductForm
 {
@@ -10,7 +17,35 @@ class ProductForm
     {
         return $schema
             ->components([
-                //
+                Wizard::make([
+                    Step::make('Product Info') 
+                        ->description('Isi informasi dasar produk') 
+                        ->schema([ 
+                            Group::make([ 
+                                TextInput::make('name')
+                                    ->required(),
+                                TextInput::make('sku')
+                                    ->required(), 
+                                ])->columns(2), 
+                                MarkdownEditor::make('description') 
+                        ]),
+                        // Step::make('Product prices')
+                        Step::make('Pricing & Stock') 
+                            ->description('Isi harga dan jumlah stok') 
+                            ->schema([ 
+                                Group::make([
+                                    TextInput::make('price') 
+                                        ->required(), 
+                                    TextInput::make('stock') 
+                                        ->required(),
+                                ])->columns(2),
+                                MarkdownEditor::make('description')
+                            ]),
+                ])
+                ->columnSpanFull()
             ]);
     }
 }
+
+
+
