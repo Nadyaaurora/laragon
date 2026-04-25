abf222d0fb8293bda2ebe9912d5e2f2c8d81f2db
nadyaaurora <nadyaaurora575@gmail.com>
2026-04-25 09:35:05 +0700

    feat : add filter

--- PraktikumPWL/app/Filament/Resources/Posts/Tables/PostsTable.php
+++ PraktikumPWL/app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -9,6 +9,9 @@ use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Columns\ColorColumn;
 use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Forms\Components\DatePicker;
+use Filament\Tables\Filters\SelectFilter;
 
 class PostsTable
 {
@@ -35,7 +38,23 @@ class PostsTable
                     ->sortable(),
             ])->defaultSort('created_at', 'asc')
             ->filters([
-                //
+                Filter::make('created_at')
+                    ->label('Creation Date')
+                        ->schema([
+                            DatePicker::make('created_at')
+                                ->label('Select Date')
+                        ])
+                        ->query(function ($query, $data){ 
+                            return $query
+                                ->when( 
+                                    $data['created_at'], 
+                                    fn ($query, $date) => $query->whereDate('created_at', $date), 
+                                ); 
+                        }),
+                SelectFilter::make('category_id') 
+                    ->label('Select Category') 
+                    ->relationship('category', 'name') 
+                    ->preload(),
             ])
             ->recordActions([
                 EditAction::make(),
